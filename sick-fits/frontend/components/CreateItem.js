7500fe2d671f2b42ed0ee3c042d320bdcdac1b24
AZReed <azreed@gmail.com>
2018-12-13 14:09:32 -0300

    form to update item

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -42,24 +42,35 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
-  uploadFile = e => {
-    console.log("uplaodFile",e);
-    const files = e.target.file
-  }
+  uploadFile = async e => {
+    console.log('uplaodFile', e);
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/dpnpibjjs/image/upload', { method: 'POST', body: data});
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
 
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
           <Form
-            onSubmit={ async e => {
+            onSubmit={async e => {
               e.preventDefault();
               const res = await createItem();
               console.log(res);
               Router.push({
                 pathname: '/item',
                 query: { id: res.data.createItem.id }
-              })
+              });
             }}
           >
             <Error error={error} />
@@ -74,6 +85,7 @@ class CreateItem extends Component {
                   required
                   onChange={this.uploadFile}
                 />
+                {this.state.image && <img width={200} src={this.state.image} alt="Upload Preview" />}
               </label>
               <label htmlFor="title">
                 Title
