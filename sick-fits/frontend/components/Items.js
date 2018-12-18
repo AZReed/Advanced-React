3ba22d2680d799180c81ea6c9491ea0a38597602
AZReed <azreed@gmail.com>
2018-12-18 13:31:52 -0300

    Pagination query

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -3,11 +3,12 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
 import Item from './Item';
-import Pagination from './Pagination'
+import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -24,7 +25,7 @@ const Center = styled.div`
 
 const ItemsList = styled.div`
   display: grid;
-  grid-auto-columns: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   grid-gap: 60px;
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
@@ -34,21 +35,26 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Pagination page={this.props.page}/>
-          <Query query={ALL_ITEMS_QUERY}>
-            {({ data, error, loading }) => {
-              if (loading) return <p>Loading...</p>;
-              if (error) return <p>Error: {error.message}</p>;
-              return (
-                <ItemsList>
-                  {data.items.map(item => (
-                    <Item key={item.id} item={item} />
-                  ))}
-                </ItemsList>
-              );
-            }}
-          </Query>
-        <Pagination page={this.props.page}/>
+        <Pagination page={this.props.page} />
+        <Query
+          query={ALL_ITEMS_QUERY}
+          variables={{
+            skip: this.props.page * perPage - perPage,
+          }}
+        >
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
+            return (
+              <ItemsList>
+                {data.items.map(item => (
+                  <Item key={item.id} item={item} />
+                ))}
+              </ItemsList>
+            );
+          }}
+        </Query>
+        <Pagination page={this.props.page} />
       </Center>
     );
   }
