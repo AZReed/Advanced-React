1ac6b1f388b6ad65663610ec1d7a3a898aa84899
AZReed <azreed@gmail.com>
2018-12-21 19:20:00 -0300

    permissions and form

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -8,14 +9,22 @@ const Query = {
     if (!ctx.request.userId) {
       return null;
     }
-    return ctx.db.query.user({
-      where: { id: ctx.request.userId }
-    }, info);
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  },
+  async users(parent, args, ctx, info) {
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in!');
+    }
+
+    hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
+
+    return ctx.db.query.users({}, info);
   }
-  /*   async items(parents, args, ctx, info) {
-    const items = await ctx.db.query.items();
-    return items;
-  } */
 };
 
 module.exports = Query;
