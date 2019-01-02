0b136f8fdd6bb8d46d2055b264dcbe11c050b4e9
AZReed <azreed@gmail.com>
2019-01-02 18:36:38 -0300

    remove cart items from server

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -239,6 +239,30 @@ const Mutations = {
       },
       info
     );
+  },
+  async removeFromCart(aprent, args, ctx, info) {
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      `{id, user { id }}`
+    );
+    if (!cartItem) {
+      throw new Error('No Cart Item Found!');
+    }
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('Cheatin huhhh');
+    }
+    return ctx.db.mutation.deleteCartItem(
+      {
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
   }
 };
 
