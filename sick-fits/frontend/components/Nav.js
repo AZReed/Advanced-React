1d5e3e4a47eb4090d547eea32d7fec43f4975f01
AZReed <azreed@gmail.com>
2019-01-04 20:14:40 -0300

    cart count and animation

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -3,7 +3,9 @@ import { Mutation } from 'react-apollo';
 import { TOGGLE_CART_MUTATION } from '../components/Cart';
 import NavStyles from './styles/NavStyles';
 import User from './User';
-import Signout from './Signout'
+import Signout from './Signout';
+import CartCount from './CartCount';
+import CartItem from './CartItem';
 
 const Nav = () => (
   <User>
@@ -27,8 +29,13 @@ const Nav = () => (
               <Signout />
             </Link>
             <Mutation mutation={TOGGLE_CART_MUTATION}>
-              {(toggleCart) => (
-                <button onClick={toggleCart}>My Cart</button>
+              {toggleCart => (
+                <button onClick={toggleCart}>
+                  My Cart
+                  <CartCount
+                    count={me.cart.reduce( (tally, cartItem) => tally + cartItem.quantity, 0 )}
+                  />
+                </button>
               )}
             </Mutation>
           </>
