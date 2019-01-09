1b0bc399c5e5c71a84e1ce90158a790181b998b4
AZReed <azreed@gmail.com>
2019-01-09 13:28:49 -0300

    order query restriction bug fix

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -39,10 +39,12 @@ const Query = {
     );
 
     const ownOrder = order.user.id === ctx.request.userId;
+    console.log(ownOrder, order.user.id, ctx.request.userId);
     const hasPermissionToSeeOrder = ctx.request.user.permissions.includes(
       'ADMIN'
     );
-    if (!ownOrder || !hasPermissionToSeeOrder) {
+  
+    if (!ownOrder && !hasPermissionToSeeOrder) {
       throw new Error('You cant see this');
     }
 
