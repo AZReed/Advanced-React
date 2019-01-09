da9f1307ff04f70a956c453fb73cacf05d1d0d06
AZReed <azreed@gmail.com>
2019-01-08 22:03:49 -0300

    orders list

--- sick-fits/frontend/components/PleaseSignin.js
+++ sick-fits/frontend/components/PleaseSignin.js
@@ -1,3 +1,4 @@
+// import React from 'react';
 import { Query } from 'react-apollo';
 import { CURRENT_USER_QUERY } from './User';
 import Signin from './Signin';
@@ -14,6 +15,17 @@ const PleaseSignin = props => (
           </div>
         );
       }
+
+      // Option 1
+/*       const childrenWithProps = React.Children.map(props.children, child =>
+        React.cloneElement(child, { userId: data.me.id })
+      );
+      return childrenWithProps; */
+
+      // Option 2
+      // return React.cloneElement(props.children, { userId: data.me.id })
+
+      // Option 3
       return props.children;
     }}
   </Query>
