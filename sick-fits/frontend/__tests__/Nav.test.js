dbfccb61fa9358dfc477422b4d6898a4a5ffb795
AZReed <azreed@gmail.com>
2019-02-05 23:47:24 -0300

    nav component test

--- sick-fits/frontend/__tests__/Nav.test.js
+++ sick-fits/frontend/__tests__/Nav.test.js
@@ -1,10 +1,10 @@
 import { mount } from 'enzyme';
-import toJSON from 'enzyme-to-json';
 import wait from 'waait';
-import PleaseSignin from '../components/PleaseSignin';
+import Nav from '../components/Nav';
 import { CURRENT_USER_QUERY } from '../components/User';
 import { MockedProvider } from 'react-apollo/test-utils';
 import { fakeItem, fakeUser } from '../lib/testUtils';
+import toJSON from 'enzyme-to-json';
 
 const notSignedInMocks = [
   {
@@ -20,32 +20,30 @@ const signedInMocks = [
   }
 ];
 
-describe('<PleaseSignin/>', () => {
-  it('renders the sign in dialog to logged out users', async () => {
+describe('<Nav />', () => {
+  it('renders a minimal nav when signed out', async () => {
     const wrapper = mount(
       <MockedProvider mocks={notSignedInMocks}>
-        <PleaseSignin />
+        <Nav />
       </MockedProvider>
-    );
+    )
     await wait();
     wrapper.update();
-    expect( wrapper.text() ).toContain('Please Sign In before Continuing');
-    expect(wrapper.find('Signin').exists()).toBe(true);
+    const nav = wrapper.find('ul[data-test="nav"]');
+    expect(toJSON(nav)).toMatchSnapshot();
   });
 
-  it('renders the child component when the user is signed in', async () => {
-    const Child = () => <p>Child Component</p>
+  it('renders full nav when signed in', async () => {
     const wrapper = mount(
       <MockedProvider mocks={signedInMocks}>
-        <PleaseSignin>
-          <Child />
-        </PleaseSignin>
+        <Nav />
       </MockedProvider>
-    );
-
+    )
     await wait();
     wrapper.update();
-    expect(wrapper.contains(<Child />)).toBe(true);
+    const nav = wrapper.find('ul[data-test="nav"]');
+    expect(nav.children().length).toBe(6);
+    expect(nav.text()).toContain('Sign Out')
+    expect(toJSON(nav)).toMatchSnapshot();
   });
-
 });
